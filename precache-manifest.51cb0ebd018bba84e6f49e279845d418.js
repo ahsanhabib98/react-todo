ad44d5c951476b49339189e1a4cd9f09f6d1ef43
ahsanhabib98 <ahredoan@gmail.com>
2020-08-09 03:30:18 +0600

    Updates

---
+++ precache-manifest.51cb0ebd018bba84e6f49e279845d418.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d65f1226f6520e1388cd8686b13ed9e7",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "3d9d80b8869edece9551",
+    "url": "/react-todo/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "90d5a82c58a9bd4f1948",
+    "url": "/react-todo/static/js/2.169ff8f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.169ff8f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d9d80b8869edece9551",
+    "url": "/react-todo/static/js/main.d1aba4f0.chunk.js"
+  },
+  {
+    "revision": "84cc2894bf9c2b9b0e41",
+    "url": "/react-todo/static/js/runtime-main.760a8a34.js"
+  }
+]);
